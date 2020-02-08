3f2587be230d478d2e6df4a5511460f6070449e5
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2020-02-08 01:22:01 -0400

    Language update

--- packages_relationship/parts/admin/help-faq-dev.php
+++ packages_relationship/parts/admin/help-faq-dev.php
@@ -1,10 +1,10 @@
 <!-- FAQ - How to custom Package Profile Information? -->
-<h2 class="render-title">How to custom Company's Package Profile Information?</h2>
+<h2 class="render-title"><?php _e("How to custom Company's Package Profile Information?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-10.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>CSS:</p>
 	<p><code>packages/assets/css/user/packages-profile-info.css</code></p>
 	<p>PHP:</p>
@@ -14,12 +14,12 @@
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - How to custom Company module? -->
-<h2 class="render-title">How to custom Company module?</h2>
+<h2 class="render-title"><?php _e("How to custom Company module?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-11.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>CSS:</p>
 	<p><code>packages/assets/css/user/packages-profile-info.css</code></p>
 	<p>PHP:</p>
@@ -29,12 +29,12 @@
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - How to custom Notifications module? -->
-<h2 class="render-title">How to custom Notifications module?</h2>
+<h2 class="render-title"><?php _e("How to custom Notifications module?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-12.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>CSS:</p>
 	<p><code>packages/assets/css/user/packages-profile-info.css</code></p>
 	<p>PHP:</p>
@@ -44,12 +44,12 @@
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - How to custom Members module? -->
-<h2 class="render-title">How to custom Members module?</h2>
+<h2 class="render-title"><?php _e("How to custom Members module?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-13.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>CSS:</p>
 	<p><code>packages/assets/css/user/packages-profile-info.css</code></p>
 	<p>PHP:</p>
@@ -59,12 +59,12 @@
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - How to custom Requests/Invitations module? -->
-<h2 class="render-title">How to custom Requests/Invitations module?</h2>
+<h2 class="render-title"><?php _e("How to custom Requests/Invitations module?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-14.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>CSS:</p>
 	<p><code>packages/assets/css/user/packages-profile-info.css</code></p>
 	<p>PHP:</p>
@@ -74,12 +74,12 @@
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - How to custom custom public profile buttons? -->
-<h2 class="render-title">How to custom the public profile buttons?</h2>
+<h2 class="render-title"><?php _e("How to custom the public profile buttons?", 'packages_relationship'); ?></h2>
 <div class="form-row float-right" style="width: 30%;">
 	<center><img src="<?php echo '../oc-content/plugins/'.PACKAGES_RELATIONSHIP_FOLDER.'assets/img/osclass-packages-help-15.jpg'; ?>"></center>
 </div>
 <div class="form-row float-left" style="width: 70%;">
-	<p><span>1)</span> Open the following files:</p>
+	<p><span>1)</span> <?php _e("Open the following files:", 'packages'); ?></p>
 	<p>PHP:</p>
 	<p><code>packages_relationship/parts/user/user-public-profile_buttons.php</code></p>
 	<br>
--- packages_relationship/parts/admin/help-faq.php
+++ packages_relationship/parts/admin/help-faq.php
@@ -1,38 +1,38 @@
 <!-- FAQ - why can't I receive invitations from Company type accounts? -->
-<h2 class="render-title">I have a User type account, why can't I receive invitations from Company type accounts?</h2>
+<h2 class="render-title"><?php _e("I have a User type account, why can't I receive invitations from Company type accounts?", 'packages_relationship'); ?></h2>
 <div class="form-row">
-	<p><span>1)</span> Go to Update account (from user logged).</p>
-	<p><span>2)</span> Mark 'Receive invitations' field.</p>
-	<p><span>3)</span> Update.</p>
+	<p><span>1)</span> <?php _e("Go to Update account (from user logged).", 'packages_relationship'); ?></p>
+	<p><span>2)</span> <?php _e("Mark 'Receive invitations' field.", 'packages_relationship'); ?></p>
+	<p><span>3)</span> <?php _e("Update.", 'packages_relationship'); ?></p>
 </div>
 
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - why can't I receive invitations from Company type accounts? -->
-<h2 class="render-title">I have a Company type account, why can't I receive requests from User type accounts?</h2>
+<h2 class="render-title"><?php _e("I have a Company type account, why can't I receive requests from User type accounts?", 'packages_relationship'); ?></h2>
 <div class="form-row">
-	<p><span>1)</span> Go to Update account (from company logged).</p>
-	<p><span>2)</span> Mark 'Receive requests' field.</p>
-	<p><span>3)</span> Update.</p>
+	<p><span>1)</span> <?php _e("Go to Update account (from company logged).", 'packages_relationship'); ?></p>
+	<p><span>2)</span> <?php _e("Mark 'Receive requests' field.", 'packages_relationship'); ?></p>
+	<p><span>3)</span> <?php _e("Update.", 'packages_relationship'); ?></p>
 </div>
 
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - Why I have not inherited the package of the company? -->
-<h2 class="render-title">I am User type account and I have joined a company, why I have not inherited the package of the company?</h2>
+<h2 class="render-title"><?php _e("I am User type account and I have joined a company, why I have not inherited the package of the company?", 'packages_relationship'); ?></h2>
 <div class="form-row">
-	<p>When a user account is a member of a company, it does not automatically inherit the package from the company.</p>
-	<p>The owner of the company's account must click on 'Inherit' to the members who wish they can use their publications package.</p>
+	<p><?php _e("When a user account is a member of a company, it does not automatically inherit the package from the company.", 'packages_relationship'); ?></p>
+	<p><?php _e("The owner of the company's account must click on 'Inherit' to the members who wish they can use their publications package.", 'packages_relationship'); ?></p>
 </div>
 
 <div style="clear: both;"><br><br></div>
 
 <!-- FAQ - Why I have not inherited the package of the company? -->
-<h2 class="render-title">I am member of a company with its package inherited, but I don't want to use it and at the same time I do not want to leave the company, what do I do?</h2>
+<h2 class="render-title"><?php _e("I am member of a company with its package inherited, but I don't want to use it and at the same time I do not want to leave the company, what do I do?", 'packages_relationship'); ?></h2>
 <div class="form-row">
-	<p><span>1)</span> Go to Update account (from user logged).</p>
-	<p><span>2)</span> Unmark 'Use package' field.</p>
-	<p><span>3)</span> Update.</p>
+	<p><span>1)</span> <?php _e("Go to Update account (from user logged).", 'packages_relationship'); ?></p>
+	<p><span>2)</span> <?php _e("Unmark 'Use package' field.", 'packages_relationship'); ?></p>
+	<p><span>3)</span> <?php _e("Update.", 'packages_relationship'); ?></p>
 </div>
 
 <div style="clear: both;"><br><br></div>
