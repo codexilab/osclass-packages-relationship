26ed27191330b10d939556033ab88d18bfd6bfbc
Adrián Olmedo <adrianolmedo.ve@gmail.com>
2020-02-12 17:38:21 -0400

    Added ABS_PATH control

--- packages_relationship/parts/admin/help-faq.php
+++ packages_relationship/parts/admin/help-faq.php
@@ -1,3 +1,20 @@
+<?php if ( ! defined('ABS_PATH')) exit('ABS_PATH is not loaded. Direct access is not allowed.');
+/*
+ * Copyright 2019 - 2020 CodexiLab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+?>
 <!-- FAQ - why can't I receive invitations from Company type accounts? -->
 <h2 class="render-title"><?php _e("I have a User type account, why can't I receive invitations from Company type accounts?", 'packages_relationship'); ?></h2>
 <div class="form-row">
